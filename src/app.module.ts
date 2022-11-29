2699b778b8bef62d308df54644594518f0d16436
Ricardo <ricar2ce@gmail.com>
2022-11-29 18:35:53 -0500

    Refactoring Users

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
